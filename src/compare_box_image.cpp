a16fdae76eea50922b85139879116db8f2426544
ccm <ccm1794@koreatech.ac.kr>
2023-08-22 10:37:26 +0900

    타이머 콜백으로 매칭시키는 버전 0822

--- src/compare_box_image.cpp
+++ src/compare_box_image.cpp
@@ -10,6 +10,7 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <chrono>
 
 #include <sensor_msgs/msg/point_cloud.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
@@ -141,7 +142,7 @@ float get_iou(const Box_yolo &a, const Box_yolo &b)
   return iou;
 }
 
-std::mutex mut_img, mut_img1, mut_img2, mut_box, mut_yolo;
+std::mutex mut_img, mut_box, mut_yolo;
 std::vector<Box> boxes;
 std::vector<Box_yolo> boxes_yolo;
 
@@ -155,8 +156,6 @@ public:
   Mat frame;
   Mat image_undistorted;
 
-  Mat yolo_overlay; // 욜로박스 오버레이
-  Mat box_overlay; // 클러스터박스 오버레이
   Mat overlay; // 전체 오버레이
 
   bool is_rec_image = false;
@@ -173,8 +172,6 @@ public:
   vector<double> CameraMat_vector;
   vector<double> DistCoeff_vector;
 
-  pthread_t tids1_;
-  // pthread_t tids2_;
 public:
   ImageLiDARFusion()
   : Node("projection_box")
@@ -223,12 +220,16 @@ public:
     publish_point_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(
       "/coord_xyz", 10);
 
+    // 타이머 콜백으로 박스 매칭함수 실행
+    auto timer_callback = [this]() -> void {FusionCallback();};
+    timer_ = create_wall_timer(80ms, timer_callback); // 10hz
+    
     RCLCPP_INFO(this->get_logger(), "------------initialize end------------\n");
   }
 
   ~ImageLiDARFusion()
   {
-    pthread_join(this->tids1_, NULL);
+    // pthread_join(this->tids1_, NULL);
     // pthread_join(this->tids2_, NULL);
   }
 
@@ -237,6 +238,7 @@ public:
   void ImageCallback(const sensor_msgs::msg::Image::SharedPtr msg);
   void YOLOCallback(const vision_msgs::msg::Detection2DArray::SharedPtr msg);
   void BoxCallback(const vision_msgs::msg::Detection3DArray::SharedPtr msg);
+  void FusionCallback();
 
 private:
   rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
@@ -244,6 +246,7 @@ private:
   rclcpp::Subscription<vision_msgs::msg::Detection3DArray>::SharedPtr lidar_box_sub_;  
   rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr publish_cone_;
   rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr publish_point_;
+  rclcpp::TimerBase::SharedPtr timer_;
 };
 
 // 파라미터 설정
@@ -287,155 +290,18 @@ void ImageLiDARFusion::ImageCallback(const sensor_msgs::msg::Image::SharedPtr ms
     RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
     return;
   }
-
+  mut_img.lock();
   cv::undistort(cv_ptr->image, this->image_undistorted, this->CameraMat, this->DistCoeff);
-  this->overlay = this->image_undistorted.clone(); // 이거 무슨뜻?
+  // this->overlay = this->image_undistorted.clone(); // 이거 무슨뜻?
+  mut_img.unlock();
 
   this->is_rec_image = true;
 
-  std::vector<Box_yolo> boxes_2d_cluster;
-
-  if(this->yolo_locker == 1 && this->box_locker == 1)
-  {
-    for (const auto& Box : boxes)
-    {
-      Box_points vertices = calcBox_points(Box);
-      double box_1[4] = {vertices.x1, vertices.y1, vertices.z1, 1.0};
-      double box_2[4] = {vertices.x2, vertices.y2, vertices.z2, 1.0};
-      double box_3[4] = {vertices.x3, vertices.y3, vertices.z3, 1.0};
-      double box_4[4] = {vertices.x4, vertices.y4, vertices.z4, 1.0};
-
-      cv::Mat pos1( 4, 1, CV_64F, box_1); // 3차원 좌표
-      cv::Mat pos2( 4, 1, CV_64F, box_2);
-      cv::Mat pos3( 4, 1, CV_64F, box_3);
-      cv::Mat pos4( 4, 1, CV_64F, box_4);
-
-      //카메라 원점 xyz 좌표 (3,1)생성
-      cv::Mat newpos1(transformMat * pos1); // 카메라 좌표로 변환한 것.
-      cv::Mat newpos2(transformMat * pos2);
-      cv::Mat newpos3(transformMat * pos3);
-      cv::Mat newpos4(transformMat * pos4);
-
-      float x1 = (float)(newpos1.at<double>(0, 0) / newpos1.at<double>(2, 0));
-      float y1 = (float)(newpos1.at<double>(1, 0) / newpos1.at<double>(2, 0));
-
-      float x2 = (float)(newpos2.at<double>(0, 0) / newpos2.at<double>(2, 0));
-      float y2 = (float)(newpos2.at<double>(1, 0) / newpos2.at<double>(2, 0));
-
-      float x3 = (float)(newpos3.at<double>(0, 0) / newpos3.at<double>(2, 0));
-      float y3 = (float)(newpos3.at<double>(1, 0) / newpos3.at<double>(2, 0));
-
-      float x4 = (float)(newpos4.at<double>(0, 0) / newpos4.at<double>(2, 0));
-      float y4 = (float)(newpos4.at<double>(1, 0) / newpos4.at<double>(2, 0));
-    
-      if (Box.y >= 0)
-      {
-        cv::rectangle(this->overlay, Rect(Point(x4, y3), Point(x2, y1)), Scalar(0, 255, 0), 2, 8, 0);
-        Box_yolo box_basic = { x4, x2, y3, y1 };
-        boxes_2d_cluster.push_back(box_basic);
-      }
-      else if (Box.y < 0)
-      {
-        cv::rectangle(this->overlay, Rect(Point(x4, y4), Point(x2, y2)), Scalar(0, 255, 0), 2, 8, 0);
-        Box_yolo box_basic = { x4, x2, y4, y2 };
-        boxes_2d_cluster.push_back(box_basic);
-      }
-    }
-
-
-
-    for (const auto& Box : boxes_yolo)
-    {
-      int xx1 = Box.x1;
-      int xx2 = Box.x2;
-      int yy1 = Box.y1;
-      int yy2 = Box.y2;
-
-      if (Box.color == 1)
-      {
-        cv::rectangle(this->overlay, Rect(Point(xx1, yy1), Point(xx2, yy2)), Scalar(0, 0, 255), 1, 8, 0);
-      }
-      else if (Box.color == 2)
-      {
-        cv::rectangle(this->overlay, Rect(Point(xx1, yy1), Point(xx2, yy2)), Scalar(255, 0, 0), 1, 8, 0);
-      }
-    }
-
-
-    float opacity = 0.6;
-    cv::addWeighted(this->overlay, opacity, this->image_undistorted, 1-opacity, 0, this->image_undistorted); // 투영된 박스를 합친다.
-
-
-    std_msgs::msg::Float32MultiArray coord;
-    sensor_msgs::msg::PointCloud2 cloud_msg;
-
-    pcl::PointCloud<pcl::PointXYZ>::Ptr coord_cloud(new pcl::PointCloud<pcl::PointXYZ>);
-
-    for ( int i =0; i < boxes_2d_cluster.size(); i++)
-    {
-      float max_iou = 0.;
-      int class_id = -1;
-
-      for ( int j = 0 ; j < boxes_yolo.size(); j++)
-      {
-        float iou = get_iou(boxes_2d_cluster[i], boxes_yolo[i]);
-        if(iou > max_iou)
-        {
-          max_iou = iou;
-          class_id = boxes_yolo[j].color;
-        }
-      }
-
-      if(max_iou > 0.1)
-      {
-        pcl::PointXYZ coord_;
-        cv::rectangle(this->image_undistorted, Rect(Point(boxes_2d_cluster[i].x1, boxes_2d_cluster[i].y1), Point(boxes_2d_cluster[i].x2, boxes_2d_cluster[i].y2)), Scalar(100, 100, 0), 3, 8, 0);
-        // 센터점을 멀티어레이에 넣고 그대로 펍?
-        float center_x = boxes[i].x;
-        float center_y = boxes[i].y;
-        // cout << "center_x : " << center_x << "   center_y : " << center_y << endl;
-        coord.data.push_back(center_x);
-        coord.data.push_back(center_y);
-        coord.data.push_back(class_id);
-        coord.data.push_back(-1000.0);
-
-        coord_.x = center_x;
-        coord_.y = center_y;
-        coord_.z = 0.;
-
-        coord_cloud->push_back(coord_); // 여기에서 코드가 멈춘다.
-      }
-    }
-    this->publish_cone_->publish(coord);
-
-    coord_cloud->width = 1;
-    coord_cloud->height = coord_cloud->points.size();
-    pcl::toROSMsg(*coord_cloud, cloud_msg);
-    cloud_msg.header.frame_id = "velodyne";
-    this->publish_point_->publish(cloud_msg);
-
-    string windowName = "overlay";
-    cv::namedWindow(windowName, 3);
-    cv::imshow(windowName, this->image_undistorted);
-    char ch = cv::waitKey(10);
-    // if(ch == 27) break;
-
-    //====== 클러스터 박스 초기화 ==========
-    this->box_locker = 0;
-    boxes.clear();
-    //==================================
-    //======== 욜로박스 초기화 ============
-    this->yolo_locker = 0;
-    boxes_yolo.clear();
-  //==================================
-  }
 }
 
 // yolo 박스 설정
 void ImageLiDARFusion::YOLOCallback(const vision_msgs::msg::Detection2DArray::SharedPtr msg)
 {
-  
-
   if (msg->detections.size() != 0 && this->yolo_locker == 0)
   {
     this->yolo_locker = 1;
@@ -476,9 +342,7 @@ void ImageLiDARFusion::YOLOCallback(const vision_msgs::msg::Detection2DArray::Sh
     this->is_rec_yolo = true;
     mut_yolo.unlock();
   }
-  else
-  {
-  }
+  else{}
 }
 
 // Box callback
@@ -513,6 +377,171 @@ void ImageLiDARFusion::BoxCallback(const vision_msgs::msg::Detection3DArray::Sha
     mut_box.unlock();    
     this->is_rec_box = true;
   }
+}
+
+// fusion callback
+void ImageLiDARFusion::FusionCallback()
+{
+  mut_img.lock();
+  this->overlay = this->image_undistorted.clone();
+  Mat frame_projected = this->image_undistorted.clone();
+  mut_img.unlock();
+
+  mut_box.lock();
+  std::vector<Box> lidar_boxes = boxes;
+  mut_box.unlock();
+
+  mut_yolo.lock();
+  std::vector<Box_yolo> yolo_boxes = boxes_yolo;
+  mut_yolo.unlock();
+
+  if (this->yolo_locker == 1 && this->box_locker == 1)
+  {
+    std::vector<Box_yolo> boxes_2d_cluster;
+
+    for (const auto& Box : lidar_boxes)
+    {
+      Box_points vertices = calcBox_points(Box);
+      double box_1[4] = {vertices.x1, vertices.y1, vertices.z1, 1.0};
+      double box_2[4] = {vertices.x2, vertices.y2, vertices.z2, 1.0};
+      double box_3[4] = {vertices.x3, vertices.y3, vertices.z3, 1.0};
+      double box_4[4] = {vertices.x4, vertices.y4, vertices.z4, 1.0};
+
+      cv::Mat pos1( 4, 1, CV_64F, box_1); // 3차원 좌표
+      cv::Mat pos2( 4, 1, CV_64F, box_2);
+      cv::Mat pos3( 4, 1, CV_64F, box_3);
+      cv::Mat pos4( 4, 1, CV_64F, box_4);
+
+      //카메라 원점 xyz 좌표 (3,1)생성
+      cv::Mat newpos1(this->transformMat * pos1); // 카메라 좌표로 변환한 것.
+      cv::Mat newpos2(this->transformMat * pos2);
+      cv::Mat newpos3(this->transformMat * pos3);
+      cv::Mat newpos4(this->transformMat * pos4);
+
+      float x1 = (float)(newpos1.at<double>(0, 0) / newpos1.at<double>(2, 0));
+      float y1 = (float)(newpos1.at<double>(1, 0) / newpos1.at<double>(2, 0));
+
+      float x2 = (float)(newpos2.at<double>(0, 0) / newpos2.at<double>(2, 0));
+      float y2 = (float)(newpos2.at<double>(1, 0) / newpos2.at<double>(2, 0));
+
+      float x3 = (float)(newpos3.at<double>(0, 0) / newpos3.at<double>(2, 0));
+      float y3 = (float)(newpos3.at<double>(1, 0) / newpos3.at<double>(2, 0));
+
+      float x4 = (float)(newpos4.at<double>(0, 0) / newpos4.at<double>(2, 0));
+      float y4 = (float)(newpos4.at<double>(1, 0) / newpos4.at<double>(2, 0));
+
+      if (Box.y >= 0)
+      {
+        cv::rectangle(this->overlay, Rect(Point(x4, y3), Point(x2, y1)), Scalar(0, 255, 0), 2, 8, 0);
+        Box_yolo box_basic = { x4, x2, y3, y1 };
+        boxes_2d_cluster.push_back(box_basic);
+      }
+      else if (Box.y < 0)
+      {
+        cv::rectangle(this->overlay, Rect(Point(x4, y4), Point(x2, y2)), Scalar(0, 255, 0), 2, 8, 0);
+        Box_yolo box_basic = { x4, x2, y4, y2 };
+        boxes_2d_cluster.push_back(box_basic);
+      }      
+    }
+
+    for (const auto& Box : yolo_boxes)
+    {
+      int xx1 = Box.x1;
+      int xx2 = Box.x2;
+      int yy1 = Box.y1;
+      int yy2 = Box.y2;
+
+      if (Box.color == 1)
+      {
+        cv::rectangle(this->overlay, Rect(Point(xx1, yy1), Point(xx2, yy2)), Scalar(0, 0, 255), 1, 8, 0);
+      }
+      else if (Box.color == 2)
+      {
+        cv::rectangle(this->overlay, Rect(Point(xx1, yy1), Point(xx2, yy2)), Scalar(255, 0, 0), 1, 8, 0);
+      }
+    }
+
+    // 이미지에 투영하는 부분
+    float opacity = 0.6;
+    cv::addWeighted(this->overlay, opacity, frame_projected, 1-opacity, 0, frame_projected); // 투영된 박스를 합친다.
+  
+    std_msgs::msg::Float32MultiArray coord;
+    sensor_msgs::msg::PointCloud2 cloud_msg;
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr coord_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+    for ( int i = 0; i < boxes_2d_cluster.size(); i++)
+    {
+      float max_iou = 0.0f;
+      int class_id = -1;
+
+      for (int j = 0; j < yolo_boxes.size(); j++)
+      {
+        float iou = get_iou(boxes_2d_cluster[i], yolo_boxes[j]);
+        if(iou > max_iou)
+        {
+          max_iou = iou;
+          class_id = yolo_boxes[j].color;
+        }
+      }
+
+      if (max_iou > 0.3f)
+      {
+        pcl::PointXYZ coord_;
+
+        cv::rectangle(frame_projected, 
+        Rect(Point(boxes_2d_cluster[i].x1, boxes_2d_cluster[i].y1), 
+        Point(boxes_2d_cluster[i].x2, boxes_2d_cluster[i].y2)), 
+        Scalar(100, 100, 0), 3, 8, 0);
+
+        float center_x = lidar_boxes[i].x; // 3차원 상대좌표에서 x,y 가져옴
+        float center_y = lidar_boxes[i].y;
+
+        // float data
+        coord.data.push_back(center_x); 
+        coord.data.push_back(center_y);
+        coord.data.push_back(class_id);
+        coord.data.push_back(-1000.0);
+
+        // pcl data
+        coord_.x = center_x;
+        coord_.y = center_y;
+        coord_.z = 0.;
+
+        coord_cloud->push_back(coord_);
+      }
+    }
+
+    // 플래닝으로 가는 데이터
+    this->publish_cone_->publish(coord);
+
+    // 포인트클라우드 퍼블리시
+    coord_cloud->width = 1;
+    coord_cloud->height = coord_cloud->points.size();
+    pcl::toROSMsg(*coord_cloud, cloud_msg);
+    cloud_msg.header.frame_id = "velodyne";
+    // cloud_msg.stamp = node->now();
+    this->publish_point_->publish(cloud_msg);
+
+    //====== 클러스터 박스 초기화 ==========
+    this->box_locker = 0;
+    boxes.clear();
+    //==================================
+    //======== 욜로박스 초기화 ============
+    this->yolo_locker = 0;
+    boxes_yolo.clear();
+    //==================================
+
+    this->is_rec_image = false;
+    this->is_rec_box = false;
+  }
+
+  //imshow
+  string windowName = "overlay";
+  cv::namedWindow(windowName, 3);
+  cv::imshow(windowName, frame_projected);
+  char ch = cv::waitKey(10);
+  // if(ch == 27) break;
 }
 
 int main(int argc, char **argv)
